47791b0dc268dbe75939807e24672cec267ddca5
Elliott Eggleston <ejegg@ejegg.com>
2017-02-02 20:16:35 -0500

    Add source fields to arrays, not just objects
    Change-Id: Ie55006deb446df96fa50e5e487b2e9559155d3c0

--- CrmLink/Messages/SourceFields.php
+++ CrmLink/Messages/SourceFields.php
@@ -4,12 +4,21 @@ namespace SmashPig\CrmLink\Messages;
 use SmashPig\Core\Context;
 
 class SourceFields {
-	public static function addToMessage( $message ) {
-		$message->source_name  = 'SmashPig';
-		$message->source_type = 'listener';
-		$message->source_host = gethostname();
-		$message->source_run_id = getmypid();
-		$message->source_version = Context::get()->getSourceRevision();
-		$message->source_enqueued_time = time();
+	public static function addToMessage( &$message ) {
+		if ( is_array( $message ) ) {
+			$message['source_name'] = 'SmashPig';
+			$message['source_type'] = 'listener';
+			$message['source_host'] = gethostname();
+			$message['source_run_id'] = getmypid();
+			$message['source_version'] = Context::get()->getSourceRevision();
+			$message['source_enqueued_time'] = time();
+		} else {
+			$message->source_name = 'SmashPig';
+			$message->source_type = 'listener';
+			$message->source_host = gethostname();
+			$message->source_run_id = getmypid();
+			$message->source_version = Context::get()->getSourceRevision();
+			$message->source_enqueued_time = time();
+		}
 	}
 }
