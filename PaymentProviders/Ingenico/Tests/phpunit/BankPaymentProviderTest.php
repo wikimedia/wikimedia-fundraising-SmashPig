945ad2c33ce86eb386c4af74adbb3e40eea75c3c
Elliott Eggleston <ejegg@ejegg.com>
2017-02-16 20:18:20 -0500

    Reduce Configuration use in BankPaymentProvider
    Provide cache params as constructor arguments
    
    Bug: T158374
    Change-Id: Ib0f5d0c1e67517c7331a2fb1bccc8dac612d603e

--- PaymentProviders/Ingenico/Tests/phpunit/BankPaymentProviderTest.php
+++ PaymentProviders/Ingenico/Tests/phpunit/BankPaymentProviderTest.php
@@ -16,18 +16,28 @@ class BankPaymentProviderTest extends BaseSmashPigUnitTestCase {
 	 */
 	protected $curlWrapper;
 
+	/**
+	 * @var BankPaymentProvider
+	 */
+	protected $provider;
+
 	public function setUp() {
 		$config = $this->setConfig( 'ingenico' );
 		$this->curlWrapper = $this->getMock( '\SmashPig\Core\Http\CurlWrapper' );
 		$config->overrideObjectInstance( 'curl/wrapper', $this->curlWrapper );
 		$config->object( 'cache' )->clear();
+		$this->provider = new BankPaymentProvider( array(
+			'cache-parameters' => array(
+				'duration' => 10,
+				'key-base' => 'BLAH_BLAH'
+			)
+		) );
 		parent::setUp();
 	}
 
 	public function testGetBankList() {
 		$this->setUpResponse( 'productDirectory', 200 );
-		$provider = new BankPaymentProvider();
-		$results = $provider->getBankList( 'NL', 'EUR' );
+		$results = $this->provider->getBankList( 'NL', 'EUR' );
 		$this->assertEquals(
 			array(
 				'INGBNL2A' => 'Issuer Simulation V3 - ING'
@@ -40,15 +50,14 @@ class BankPaymentProviderTest extends BaseSmashPigUnitTestCase {
 		$this->setUpResponse( 'productDirectory', 200 );
 		$this->curlWrapper->expects( $this->once() )
 			->method( 'execute' );
-		$provider = new BankPaymentProvider();
-		$results = $provider->getBankList( 'NL', 'EUR' );
+		$results = $this->provider->getBankList( 'NL', 'EUR' );
 		$this->assertEquals(
 			array(
 				'INGBNL2A' => 'Issuer Simulation V3 - ING'
 			),
 			$results
 		);
-		$cachedResults = $provider->getBankList( 'NL', 'EUR' );
+		$cachedResults = $this->provider->getBankList( 'NL', 'EUR' );
 		$this->assertEquals( $results, $cachedResults );
 	}
 
