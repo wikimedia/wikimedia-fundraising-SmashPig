650c2047f8620d77540bd650e7b4768b7ce09abb
Elliott Eggleston <ejegg@ejegg.com>
2017-09-11 09:38:44 -0500

    Move PaymentError and ValidationError to SmashPig
    Bug: T163868
    Change-Id: I6c07df1c322aba1e8d79855203d877fc922d98aa

---
+++ Core/ValidationError.php
@@ -0,0 +1,35 @@
+<?php
+namespace SmashPig\Core;
+
+/**
+ * Represents a validation error associated with a field or the 'general' bucket.
+ */
+class ValidationError {
+	protected $field;
+	protected $messageKey;
+	protected $messageParams;
+
+	/**
+	 * ValidationError constructor.
+	 * @param string $field normalized field name
+	 * @param string $messageKey i18n key for the error message
+	 * @param array $messageParams parameters to interpolate into the message
+	 */
+	public function __construct( $field, $messageKey, $messageParams = array() ) {
+		$this->field = $field;
+		$this->messageKey = $messageKey;
+		$this->messageParams = $messageParams;
+	}
+
+	public function getField() {
+		return $this->field;
+	}
+
+	public function getMessageKey() {
+		return $this->messageKey;
+	}
+
+	public function getMessageParams() {
+		return $this->messageParams;
+	}
+}
