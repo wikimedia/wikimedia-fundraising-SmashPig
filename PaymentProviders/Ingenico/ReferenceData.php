e5d392c270ec073114640e02d297585088098dbb
Elliott Eggleston <ejegg@ejegg.com>
2018-10-04 14:47:19 -0500

    Add some missing Ingenico card payment products
    These are the ones that we already have a mapping for in CiviCRM.
    All we really need for now is visa-electron, but I wanted to be
    a bit forward looking.
    
    Bug: T205481
    Change-Id: I95f8f28cbae6e9d3fe99978ab7a23c6144264285

--- PaymentProviders/Ingenico/ReferenceData.php
+++ PaymentProviders/Ingenico/ReferenceData.php
@@ -6,18 +6,31 @@ use OutOfBoundsException;
 
 class ReferenceData {
 	// FIXME: replace this whole class with payment_(sub)method.yaml files
+	// See https://epayments-api.developer-ingenico.com/s2sapi/v1/en_US/java/paymentproducts.html
+	// Also see wmf_civicrm.module, lines 618-699
 
 	protected static $methods = [
 		'1' => [ 'payment_method' => 'cc', 'payment_submethod' => 'visa' ],
 		'2' => [ 'payment_method' => 'cc', 'payment_submethod' => 'amex' ],
 		'3' => [ 'payment_method' => 'cc', 'payment_submethod' => 'mc' ],
 		'11' => [ 'payment_method' => 'bt', 'payment_submethod' => 'bt' ],
+		'114' => [ 'payment_method' => 'cc', 'payment_submethod' => 'visa-debit' ],
 		'117' => [ 'payment_method' => 'cc', 'payment_submethod' => 'maestro' ],
 		'118' => [ 'payment_method' => 'cc', 'payment_submethod' => 'solo' ],
+		'119' => [ 'payment_method' => 'cc', 'payment_submethod' => 'mc-debit' ],
+		'122' => [ 'payment_method' => 'cc', 'payment_submethod' => 'visa-electron' ],
 		'124' => [ 'payment_method' => 'cc', 'payment_submethod' => 'laser' ],
 		'125' => [ 'payment_method' => 'cc', 'payment_submethod' => 'jcb' ],
 		'128' => [ 'payment_method' => 'cc', 'payment_submethod' => 'discover' ],
 		'130' => [ 'payment_method' => 'cc', 'payment_submethod' => 'cb' ],
+		'132' => [ 'payment_method' => 'cc', 'payment_submethod' => 'diners' ],
+		'135' => [ 'payment_method' => 'cc', 'payment_submethod' => 'cabal' ],
+		'136' => [ 'payment_method' => 'cc', 'payment_submethod' => 'naranja' ],
+		'140' => [ 'payment_method' => 'cc', 'payment_submethod' => 'argen' ],
+		'145' => [ 'payment_method' => 'cc', 'payment_submethod' => 'nativa' ],
+		'147' => [ 'payment_method' => 'cc', 'payment_submethod' => 'elo' ],
+		'148' => [ 'payment_method' => 'cc', 'payment_submethod' => 'hiper' ],
+		'149' => [ 'payment_method' => 'cc', 'payment_submethod' => 'shopping' ],
 		'500' => [ 'payment_method' => 'obt', 'payment_submethod' => 'bpay' ],
 		'701' => [ 'payment_method' => 'dd', 'payment_submethod' => 'dd_nl' ],
 		'702' => [ 'payment_method' => 'dd', 'payment_submethod' => 'dd_de' ],
