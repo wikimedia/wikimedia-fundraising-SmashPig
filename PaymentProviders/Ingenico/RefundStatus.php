61adf13f68d2a55724a5127797fb75e4046e43df
Elliott Eggleston <ejegg@ejegg.com>
2019-01-03 18:03:54 -0500

    Batch refund script for Ingenico
    To run:
    
    php PaymentProviders/Ingenico/Maintenance/RefundPayments.php file.csv
    
    Where file.csv has columns 'payment_id', 'amount', 'currency', 'country',
    'first_name', 'last_name', and 'order_id'.
    
    Bug: T212727
    Change-Id: Ia8d37378d51f33f1a51ebb62de1c007b586075ef

---
+++ PaymentProviders/Ingenico/RefundStatus.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SmashPig\PaymentProviders\Ingenico;
+
+use OutOfBoundsException;
+use SmashPig\CrmLink\FinalStatus;
+
+class RefundStatus {
+
+	// Refund created
+	const CREATED = 'CREATED';
+	// We cancelled the refund
+	const CANCELLED = 'CANCELLED';
+	// Refund rejected
+	const REJECTED = 'REJECTED';
+	// We need to approve the refund
+	const PENDING_APPROVAL = 'PENDING_APPROVAL';
+	// Waiting for Ingenico to refund it
+	const REFUND_REQUESTED = 'REFUND_REQUESTED';
+	// Maps to 'COMPLETED' statusCategory, but does this mean refunded?
+	const CAPTURED = 'CAPTURED';
+	// Ingenico has successfully refunded the donor's money
+	const REFUNDED = 'REFUNDED';
+
+	protected static $statusMap = [
+		FinalStatus::CANCELLED => [
+			self::CANCELLED,
+		],
+		FinalStatus::COMPLETE => [
+			self::CAPTURED,
+		],
+		FinalStatus::FAILED => [
+			self::REJECTED,
+		],
+		FinalStatus::PENDING => [
+			self::REFUND_REQUESTED,
+		],
+		FinalStatus::PENDING_POKE => [
+			self::PENDING_APPROVAL,
+			self::CREATED,
+		],
+		FinalStatus::REFUNDED => [
+			self::REFUNDED,
+		]
+	];
+
+	public function normalizeStatus( $ingenicoStatus ) {
+		foreach ( self::$statusMap as $finalStatus => $ingenicoStatuses ) {
+			if ( array_search( $ingenicoStatus, $ingenicoStatuses, true ) !== false ) {
+				return $finalStatus;
+			}
+		}
+		throw new OutOfBoundsException( "Unknown Ingenico status code $ingenicoStatus" );
+	}
+}
