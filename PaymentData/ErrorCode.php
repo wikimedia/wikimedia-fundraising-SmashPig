4a18da496e70e806bd75856a2e1e7c6950bfeb0d
Elliott Eggleston <ejegg@ejegg.com>
2020-03-02 12:15:19 -0500

    Map errors on Ingenico createPayment response
    Mostly referring to the case statement in DonationInterface's
    globalcollect.adapter.php to classify the Ingenico error codes.
    
    Bug: T243340
    Change-Id: Ia5c8fe3b16012ac7c2258b6804610e2a3fe2a52d

--- PaymentData/ErrorCode.php
+++ PaymentData/ErrorCode.php
@@ -10,6 +10,8 @@ namespace SmashPig\PaymentData;
 class ErrorCode {
 	/** @var string API authentication failure */
 	const BAD_SIGNATURE = 'BAD_SIGNATURE';
+	/** @var string Normal decline code. FIXME this isn't really an error, is it? */
+	const DECLINED = 'DECLINED';
 	/** @var string Card declined on suspected villainy - DO NOT RETRY! */
 	const DECLINED_DO_NOT_RETRY = 'DECLINED_DO_NOT_RETRY';
 	/** @var string We screwed up and reused our identifier. Can increment and retry. */
@@ -20,6 +22,8 @@ class ErrorCode {
 	const MISSING_REQUIRED_DATA = 'MISSING_REQUIRED_DATA';
 	/** @var string The whole dang response is missing */
 	const NO_RESPONSE = 'NO_RESPONSE';
+	/** @var string Their server tells us that it has timed out */
+	const SERVER_TIMEOUT = 'SERVER_TIMEOUT';
 	/** @var string A supposedly well-defined field has a value we don't know what to do with */
 	const UNEXPECTED_VALUE = 'UNEXPECTED_VALUE';
 	/** @var string For use in default: cases. When encountered, classify the error and add here if needed */
