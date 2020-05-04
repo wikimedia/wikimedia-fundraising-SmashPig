04fca5bba9800b6fedc7e38f422982e8e7071f96
Elliott Eggleston <ejegg@ejegg.com>
2020-05-04 11:39:32 -0500

    Adyen iDEAL audit parse test
    Make sure payment_submethod is being populated correctly
    
    Bug: T251025
    Change-Id: I4ae40b00c8c114b160d9c6ec850ec5db10ba6534

--- PaymentProviders/Adyen/Tests/phpunit/AuditTest.php
+++ PaymentProviders/Adyen/Tests/phpunit/AuditTest.php
@@ -38,6 +38,32 @@ class AuditTest extends BaseSmashPigUnitTestCase {
 	}
 
 	/**
+	 * iDEAL donation with variant that we should discard
+	 */
+	public function testProcessDonationIdeal() {
+		$processor = new AdyenAudit();
+		$output = $processor->parseFile( __DIR__ . '/../Data/donation-ideal.csv' );
+		$this->assertEquals( 1, count( $output ), 'Should have found one donation' );
+		$actual = $output[0];
+		$expected = [
+			'gateway' => 'adyen',
+			'gross' => '5.35',
+			'contribution_tracking_id' => '80188432',
+			'currency' => 'EUR',
+			'gateway_txn_id' => '1515876691993221',
+			'invoice_id' => '80188432.1',
+			'payment_method' => 'rtbt',
+			'payment_submethod' => 'rtbt_ideal',
+			'date' => 1582488844,
+			'settled_currency' => 'USD',
+			'fee' => '0.25',
+			'settled_gross' => '5.43',
+			'settled_fee' => '0.27',
+		];
+		$this->assertEquals( $expected, $actual, 'Did not parse donation correctly' );
+	}
+
+	/**
 	 * Now try a refund
 	 */
 	public function testProcessRefund() {
