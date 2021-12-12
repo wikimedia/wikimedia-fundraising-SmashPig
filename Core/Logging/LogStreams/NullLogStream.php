04fed588400c0b336f2aedee866bf514cdc021f0
Elliott Eggleston <ejegg@ejegg.com>
2021-12-11 22:53:24 -0500

    Add no-op log stream
    Because of how the configuration cascades, we can't actually remove
    elements on a provider-specific basis from the list of enabled-log-
    streams defined in provider-defaults.yaml. This lets you at least
    disable one of them by replacing the configured class with a class
    that does nothing.
    
    Bug: T297552
    Change-Id: I6296b8ca0447028c44793d0a875197e3f85ceaa9

---
+++ Core/Logging/LogStreams/NullLogStream.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SmashPig\Core\Logging\LogStreams;
+
+use SmashPig\Core\Logging\LogContextHandler;
+use SmashPig\Core\Logging\LogEvent;
+
+class NullLogStream implements ILogStream {
+
+	public function processEvent( LogEvent $event ) {
+		// do nothing
+	}
+
+	public function registerContextHandler( LogContextHandler $ch ) {
+		// twiddle thumbs
+	}
+
+	public function enterContext( $contextNames ) {
+		// whistle tunelessly, tap feet
+	}
+
+	public function renameContext( $contextNames, $oldTopName ) {
+		// play solitaire
+	}
+
+	public function leaveContext( $contextNames ) {
+		// ...or snake
+	}
+
+	public function shutdown() {
+		// remember when every phone came with snake?
+	}
+}
