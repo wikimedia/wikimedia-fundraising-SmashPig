5e7ffdce8fa4b4c2646378695ffab2d2b116d4bd
jgleeson <jgleeson@wikimedia.org>
2023-01-05 13:50:35 +0000

    Add dLocal API request headers
    Bug: T324274
    Change-Id: Idb5e653f67a841b7c3dea5e79af97961bc019578

--- PaymentProviders/dlocal/Api.php
+++ PaymentProviders/dlocal/Api.php
@@ -2,6 +2,8 @@
 
 namespace SmashPig\PaymentProviders\dlocal;
 
+use SmashPig\Core\Http\OutboundRequest;
+
 class Api {
 
 	/**
@@ -24,14 +26,58 @@ class Api {
 	 */
 	protected $secret;
 
+	/**
+	 * dLocal API Version.
+	 *
+	 * 2.1 is the current version.
+	 *
+	 * @var string
+	 */
+	protected $version;
+
 	public function __construct( array $params ) {
 		$this->endpoint = $params['endpoint'];
 		$this->login = $params['login'];
 		$this->trans_key = $params['trans_key'];
 		$this->secret = $params['secret'];
+		$this->version = $params['version'];
 	}
 
-	protected function makeApiCall() {
-		// TODO: Implement makeApiCall() method.
+	/**
+	 * @param array $params
+	 * @return array
+	 */
+	public function makeApiCall( array $params ): array {
+		$request = $this->getNewRequest();
+		$this->setRequestHeaders( $request );
+		$request->setBody( $params );
+		return $request->execute();
 	}
+
+	/**
+	 * Set dLocal request headers
+	 * https://docs.dlocal.com/reference/payins-security#headers
+	 *
+	 * @param OutboundRequest $request
+	 * @return void
+	 */
+	protected function setRequestHeaders( OutboundRequest $request ): void {
+		// set date header using ATOM format which is ISO8601 compatible
+		$date = ( new \DateTime() )->format( \DateTime::ATOM );
+		$request->setHeader( 'X-Date', $date )
+			->setHeader( 'X-Login', $this->login )
+			->setHeader( 'X-Trans-Key', $this->trans_key )
+			->setHeader( 'Content-Type', 'application/json' )
+			->setHeader( 'X-Version', $this->version )
+			->setHeader( 'User-Agent', 'SmashPig' )
+			->setHeader( 'Authorization', '' );
+	}
+
+	/**
+	 * @return OutboundRequest
+	 */
+	protected function getNewRequest(): OutboundRequest {
+		return new OutboundRequest( $this->endpoint );
+	}
+
 }
--- PaymentProviders/dlocal/Tests/phpunit/ApiTest.php
+++ PaymentProviders/dlocal/Tests/phpunit/ApiTest.php
@@ -2,21 +2,64 @@
 
 namespace SmashPig\PaymentProviders\dlocal\Tests;
 
-use PHPUnit\Framework\TestCase;
 use SmashPig\PaymentProviders\dlocal\Api;
+use SmashPig\Tests\BaseSmashPigUnitTestCase;
 
-class ApiTest extends TestCase {
+class ApiTest extends BaseSmashPigUnitTestCase {
 
-	public function testCanInstantiateApi(): void {
-		$params = [
+	/**
+	 * @var Api
+	 */
+	public $api;
+
+	public function setUp(): void {
+		parent::setUp();
+		$testingProviderConfiguration = $this->setProviderConfiguration( 'dlocal' );
+		$testingProviderConfiguration->overrideObjectInstance( 'curl/wrapper', $this->curlWrapper );
+
+		$this->api = new Api( [
 			'endpoint' => 'http://example.com',
 			'login' => 'test_login',
 			'trans_key' => 'test_dg$3434534E',
-			'secret' => 'test_ITSASECRET'
-		];
+			'secret' => 'test_ITSASECRET',
+			'version' => '2.1'
+		] );
+	}
 
-		$Api = new Api( $params );
-		$this->assertInstanceOf( \SmashPig\PaymentProviders\dlocal\Api::class, $Api );
+	/**
+	 * TODO: This test should be moved and the visibility of Api::makeApiCall()
+	 * changed to protected once we implement the first PaymentProvider action
+	 * which internally call Api::makeApiCall().
+	 *
+	 * For now, this test confirms the behaviour of the code available.
+	 *
+	 * @return void
+	 */
+	public function testApiCallSetsRequiredRequestHeaders(): void {
+		// curlWrapper::execute() is called within Api::makeApiCall()
+		// via OutboundRequest::execute(). I did consider mocking
+		// OutboundRequest, but it looks like we typically mock the
+		// CurlWrapper for this scenario, which is one level lower.
+		$this->curlWrapper->expects( $this->once() )
+			->method( 'execute' )
+			->with(
+				$this->anything(), // url
+				$this->anything(), // method
+				$this->callback( function ( $headers ) {
+					$this->assertArrayHasKey( 'X-Date', $headers );
+					$this->assertArrayHasKey( 'X-Login', $headers );
+					$this->assertArrayHasKey( 'X-Trans-Key', $headers );
+					$this->assertArrayHasKey( 'Content-Type', $headers );
+					$this->assertArrayHasKey( 'X-Version', $headers );
+					$this->assertArrayHasKey( 'User-Agent', $headers );
+					$this->assertArrayHasKey( 'Authorization', $headers );
+					return true; // if we get here, the headers were set.
+				} )
+			)
+			->willReturn( $emptyResult = [] );
+
+		// headers are generated during the call to makeApiCall
+		$this->api->makeApiCall( $emptyParams = [] );
 	}
 
 }
