f73d04b10cc1b8602ac670965972a86cb7746c96
Damilare Adedoyin <dadedoyin@wikimedia.org>
2023-01-26 14:56:56 +0000

    Implement createHostedPayment for DLocal REDIRECT methods in SmashPig
    To test the actual API request:
    
    Replace the const variables in the Health Check Script with this values:
    
    public const HEALTHCHECK_METHOD = 'authorizePayment';
    
    public const HEALTHCHECK_PARAM = [
    		'order_id' => '#UNIQUE_ORDER_ID',
    		'amount' => '100',
    		'currency' => 'USD',
    		'country' => 'BR',
    		'payment_method_flow' => 'REDIRECT',
    		'first_name' => 'Lorem',
    		'last_name' => 'Ipsum',
    		'email' => 'test@example.com',
    		'fiscal_number' => '504.141.611-73',
    		'contact_id' => '12345',
    		'state_province' => 'lore',
    		'city' => 'lore',
    		'postal_code' => 'lore',
    		'street_address' => 'lore',
    		'street_number' => 2,
    		'user_ip' => '127.0.0.1',
    	];
    
    In the smashpig container, run:  php PaymentProviders/dlocal/Maintenance/HealthCheck.php
    
    This should return a response like this:
    
    Array
    (
        [id] => D-2486-a2ecb177-c5cc-4d84-ac9a-f983abcf49b4
        [payment_method_flow] => REDIRECT
        [created_date] => 2023-01-30T12:53:21.000+0000
        [status] => PENDING
        [status_detail] => The payment is pending.
        [status_code] => 100
        [order_id] => 891.1
        [redirect_url] => https://sandbox.dlocal.com/collect/select_payment_method?id=M-faeefc04-bdf7-4a72-9d8a-8af234dc4b26&xtid=CATH-ST-1675083201-1103267419
    )
    
    Copy the redirect_url to the browser to complete the payment.
    
    Bug: T324289
    Change-Id: I8d9bcbf415c17e320a448d165275caf806cb5d6e

--- PaymentProviders/dlocal/PaymentProvider.php
+++ PaymentProviders/dlocal/PaymentProvider.php
@@ -2,12 +2,9 @@
 
 namespace SmashPig\PaymentProviders\dlocal;
 
-use Psr\Log\LogLevel;
 use SmashPig\Core\Context;
-use SmashPig\Core\Logging\Logger;
-use SmashPig\Core\PaymentError;
-use SmashPig\PaymentData\ErrorCode;
-use SmashPig\PaymentData\FinalStatus;
+use SmashPig\Core\ProviderConfiguration;
+use SmashPig\Core\ValidationError;
 use SmashPig\PaymentProviders\Responses\ApprovePaymentResponse;
 use SmashPig\PaymentProviders\Responses\CreatePaymentResponse;
 use SmashPig\PaymentProviders\Responses\PaymentProviderResponse;
@@ -19,65 +16,13 @@ class PaymentProvider {
 	protected $api;
 
 	/**
-	 * @var \SmashPig\Core\ProviderConfiguration
+	 * @var ProviderConfiguration
 	 */
 	protected $providerConfiguration;
 
 	public function __construct() {
 		$this->providerConfiguration = Context::get()->getProviderConfiguration();
 		$this->api = $this->providerConfiguration->object( 'api' );
-	}
-
-	protected function getCreatePaymentStatusNormalizer() {
-		return new CreatePaymentStatus();
-	}
-
-	protected function mapStatusAndAddErrorsIfAny(
-			PaymentProviderResponse $response,
-			array $successfulStatuses = [ FinalStatus::PENDING_POKE, FinalStatus::COMPLETE ]
-	): void {
-		$statusMapper = $this->getCreatePaymentStatusNormalizer();
-		$rawStatus = $response->getRawStatus();
-		$rawResponse = $response->getRawResponse();
-
-		if ( $rawStatus ) {
-			try {
-				$status = $statusMapper->normalizeStatus( $rawStatus );
-				$response->setStatus( $status );
-				if ( $status === FinalStatus::FAILED ) {
-					$response->addErrors( new PaymentError(
-							ErrorMapper::$errorCodes[$rawResponse['status_code']],
-							$rawResponse['status_detail'],
-							LogLevel::ERROR
-					) );
-				} else {
-					$success = in_array( $status, $successfulStatuses );
-					$response->setSuccessful( $success );
-				}
-			} catch ( \Exception $ex ) {
-				$response->addErrors( new PaymentError(
-						ErrorCode::UNEXPECTED_VALUE,
-						$ex->getMessage(),
-						LogLevel::ERROR
-				) );
-				Logger::debug( 'Unable to map dlocal status', $rawResponse );
-			}
-		} else {
-				$message = 'Missing dlocal status';
-				$response->addErrors(
-						new PaymentError(
-								ErrorCode::MISSING_REQUIRED_DATA,
-								$message,
-								LogLevel::ERROR
-						)
-				);
-				Logger::debug( $message, $rawResponse );
-		}
-
-		if ( $response->hasErrors() ) {
-			$response->setSuccessful( false );
-			$response->setStatus( FinalStatus::FAILED );
-		}
 	}
 
 	public function createPayment( array $params ): CreatePaymentResponse {
@@ -87,4 +32,36 @@ class PaymentProvider {
 	public function approvePayment( array $params ): ApprovePaymentResponse {
 		// TODO: Implement approvePayment() method.
 	}
+
+	/**
+	 * @throws ValidationException
+	 */
+	protected static function checkFields( $requiredFields, $input ) {
+		$invalidFields = [];
+		foreach ( $requiredFields as $field ) {
+			if ( empty( $input[ $field ] ) ) {
+				$invalidFields[$field] = 'required';
+			}
+		}
+
+		if ( count( $invalidFields ) ) {
+			throw new ValidationException( "Invalid input", $invalidFields );
+		}
+	}
+
+	/**
+	 * @param array $missingParams
+	 * @param PaymentProviderResponse $paymentResponse
+	 * @return void
+	 */
+	protected function addPaymentResponseValidationErrors(
+		array $params, PaymentProviderResponse $paymentResponse
+	): void {
+		foreach ( $params as $param => $message ) {
+			$paymentResponse->addValidationError(
+				new ValidationError( $param, null, [], $message )
+			);
+		}
+	}
+
 }
