df9eeaf2b24b4c6e4c3638e47bce95f28693e279
jgleeson <jgleeson@wikimedia.org>
2023-01-27 14:50:55 +0000

    Map GatewayTxnId to Response during ApprovePayment call
    Bug: T324281
    Change-Id: If6dc84baf6ea96509a9bb83435a0e67caee0e8ce

--- PaymentProviders/dlocal/DlocalApprovePaymentResponseFactory.php
+++ PaymentProviders/dlocal/DlocalApprovePaymentResponseFactory.php
@@ -15,18 +15,31 @@ class DlocalApprovePaymentResponseFactory extends ApprovePaymentResponseFactory 
 	public static function fromRawResponse( $rawResponse ): ApprovePaymentResponse {
 		$approvePaymentResponse = new ApprovePaymentResponse();
 		$approvePaymentResponse->setRawResponse( $rawResponse );
+		$gatewayTxnId = $rawResponse['id'] ?? null;
+		if ( $gatewayTxnId ) {
+			$approvePaymentResponse->setGatewayTxnId( $gatewayTxnId );
+		}
 		$rawStatus = $rawResponse['status'] ?? null;
 		if ( $rawStatus ) {
-			$approvePaymentResponse->setRawStatus( $rawStatus );
-			$approvePaymentStatusNormalizer = new ApprovePaymentStatusNormalizer();
-			$normalizedStatus = $approvePaymentStatusNormalizer->normalizeStatus( $rawStatus );
-			$approvePaymentResponse->setStatus( $normalizedStatus );
-			$isSuccessfulStatus = $approvePaymentStatusNormalizer->isSuccessStatus( $rawStatus );
-			$approvePaymentResponse->setSuccessful( $isSuccessfulStatus );
+			self::setStatusDetails( $approvePaymentResponse, $rawStatus );
 		} else {
 			$approvePaymentResponse->setStatus( FinalStatus::UNKNOWN );
 			$approvePaymentResponse->setSuccessful( false );
 		}
 		return $approvePaymentResponse;
+	}
+
+	/**
+	 * @param ApprovePaymentResponse $approvePaymentResponse
+	 * @param string|null $rawStatus
+	 * @return void
+	 */
+	protected static function setStatusDetails( ApprovePaymentResponse $approvePaymentResponse, ?string $rawStatus ): void {
+		$approvePaymentResponse->setRawStatus( $rawStatus );
+		$approvePaymentStatusNormalizer = new ApprovePaymentStatusNormalizer();
+		$normalizedStatus = $approvePaymentStatusNormalizer->normalizeStatus( $rawStatus );
+		$approvePaymentResponse->setStatus( $normalizedStatus );
+		$isSuccessfulStatus = $approvePaymentStatusNormalizer->isSuccessStatus( $rawStatus );
+		$approvePaymentResponse->setSuccessful( $isSuccessfulStatus );
 	}
 }
