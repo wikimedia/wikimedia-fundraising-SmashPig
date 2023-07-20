aa78c545917a76dff02f18e660487e68b562490c
wfan <wfan@wikimedia.org>
2023-07-19 23:13:55 -0700

    Complete technical work to enable Venmo through Braintree
    delete unused chargepayment graphql and combine venmo and braintree gateway
    
    Test:
    Charge paypal for recurring/non-recurring, AUD and USD, and some CNY (should invalid)
    Charge venmo for recurring/non-recurring, USD and non-USD (should invalid)
    test no device data
    ./scripts/smashpig-phpunit.sh --filter testPaymentWithNoDeviceDataError
    ./scripts/smashpig-phpunit.sh --filter testVenmoNotUSDError
    
    Bug: T334430
    Change-Id: I59f064d32ccab1b5ac2b291600e510c906066b49

--- PaymentProviders/Braintree/VenmoPaymentProvider.php
+++ PaymentProviders/Braintree/VenmoPaymentProvider.php
@@ -2,50 +2,18 @@
 
 namespace SmashPig\PaymentProviders\Braintree;
 
-use SmashPig\Core\ValidationError;
-use SmashPig\PaymentData\FinalStatus;
-use SmashPig\PaymentProviders\Responses\CreatePaymentResponse;
-
 class VenmoPaymentProvider extends PaymentProvider {
 
-	protected function getInvalidParams( array $params ): array {
-		$invalidParams = [];
-		if ( empty( $params['payment_token'] ) ) {
-			$invalidParams[] = 'payment_token';
+	protected function getInvalidCurrency( $currency ) {
+		// venmo only supported by USD account
+		if ( !empty( $currency ) && $currency !== 'USD' ) {
+			return 'currency';
 		}
-		if ( empty( $params['order_id'] ) ) {
-			$invalidParams[] = 'order_id';
-		}
-		if ( empty( $params['amount'] ) ) {
-			$invalidParams[] = 'amount';
-		}
-		if ( empty( $params['device_data'] ) ) {
-			$invalidParams[] = 'device_data';
-		}
-		// currency USD is the only supported currency
-		if ( empty( $params['currency'] ) || 'USD' !== $params['currency'] ) {
-			$invalidParams[] = 'currency';
-		}
-		return $invalidParams;
+		return null;
 	}
 
-	public function createPayment( array $params ): CreatePaymentResponse {
-		$invalidParams = $this->getInvalidParams( $params );
-		$response = new CreatePaymentResponse();
-		// Get ValidationError from transformToApiParams if currency not supported
-		if ( count( $invalidParams ) > 0 ) {
-			$response->setSuccessful( false );
-			$response->setStatus( FinalStatus::FAILED );
-			foreach ( $invalidParams as $invalidParam ) {
-				$response->addValidationError(
-					new ValidationError( $invalidParam,
-						null, [],
-						'Invalid ' . $invalidParam )
-				);
-			}
-		} else {
-			// todo
-		}
-		return $response;
+	protected function indicateMerchant( array $params, array &$apiParams ) {
+		// multi currency depends on different merchant, no need for venmo yet since only one account supported
+		return $apiParams;
 	}
 }
