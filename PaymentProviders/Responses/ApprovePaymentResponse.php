8475c24c8410382541fd9c552a80774d0ef834fc
Elliott Eggleston <ejegg@ejegg.com>
2024-08-28 10:16:51 -0400

    Add getters and setters for new Approve props
    Change-Id: Ibe44e438a9eaf9ca1c0d611b8e29e57cb491c9d1

--- PaymentProviders/Responses/ApprovePaymentResponse.php
+++ PaymentProviders/Responses/ApprovePaymentResponse.php
@@ -30,4 +30,40 @@ class ApprovePaymentResponse extends PaymentProviderResponse {
 	 * @var string|null
 	 */
 	protected ?string $feeCurrency = null;
+
+	public function setSettledAmount( ?float $settledAmount ): ApprovePaymentResponse {
+		$this->settledAmount = $settledAmount;
+		return $this;
+	}
+
+	public function getSettledAmount(): ?float {
+		return $this->settledAmount;
+	}
+
+	public function setSettledCurrency( ?string $settledCurrency ): ApprovePaymentResponse {
+		$this->settledCurrency = $settledCurrency;
+		return $this;
+	}
+
+	public function getSettledCurrency(): ?string {
+		return $this->settledCurrency;
+	}
+
+	public function setFee( ?float $fee ): ApprovePaymentResponse {
+		$this->fee = $fee;
+		return $this;
+	}
+
+	public function getFee(): ?float {
+		return $this->fee;
+	}
+
+	public function setFeeCurrency( ?string $feeCurrency ): ApprovePaymentResponse {
+		$this->feeCurrency = $feeCurrency;
+		return $this;
+	}
+
+	public function getFeeCurrency(): ?string {
+		return $this->feeCurrency;
+	}
 }
