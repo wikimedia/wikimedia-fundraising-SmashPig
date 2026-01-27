d00d1c285320e56030c4c71abf2b55e029bac813
eileen <emcnaughton@wikimedia.org>
2026-01-28 10:52:42 +1300

    Bubble up grant_provider from paypal trr files
    Bug: T414999
    Change-Id: Ice9d400e4718b1825d9c9940d0eb10282b0b64fc

--- PaymentProviders/PayPal/Audit/TRRFileParser.php
+++ PaymentProviders/PayPal/Audit/TRRFileParser.php
@@ -71,6 +71,7 @@ class TRRFileParser extends BaseParser {
 			'payment_submethod' => $this->row['Card Type'] ?? null,
 			'order_id' => $this->getOrderID(),
 			'contribution_tracking_id' => $this->getContributionTrackingId(),
+			'grant_provider' => str_contains( $this->row['Transaction Subject'] ?? '', ' has sent you money' ) ? str_replace( ' has sent you money', '', $this->row['Transaction Subject'] ) : '',
 		];
 
 		return $msg + $this->getGravyFields() + $this->getRecurringFields() + $this->getReversalFields();
