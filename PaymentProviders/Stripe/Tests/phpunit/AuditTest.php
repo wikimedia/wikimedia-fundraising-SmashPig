9f64d0a2d10508d7dc03b15353151574fc1cddb1
eileen <emcnaughton@wikimedia.org>
2026-04-02 12:52:31 +1300

    Remove test that is not worth maintaining
    This test was auto-written for an alternate approach but it doesn't feel relevatn
    enough to tweak the test data for the latest changes - remove it
    
    Change-Id: I117e86e9df9226f27488ca0922e48d6adddd9a5c

--- PaymentProviders/Stripe/Tests/phpunit/AuditTest.php
+++ PaymentProviders/Stripe/Tests/phpunit/AuditTest.php
@@ -59,20 +59,6 @@ class AuditTest extends BaseSmashPigUnitTestCase {
 		$this->assertSame( 'payout', $output[5]['type'] );
 	}
 
-	public function testParseSettlementApiCsv(): void {
-		$processor = new StripeAudit();
-		$output = $processor->parseFile( __DIR__ . '/../Data/settlement_api.csv' );
-
-		$this->assertCount( 2, $output );
-		$this->assertSame( '24315.1', $output[0]['order_id'] );
-		$this->assertSame( 'pi_123', $output[0]['backend_processor_txn_id'] );
-		$this->assertSame( 'stripe', $output[0]['backend_processor'] );
-		$this->assertSame( 'acct_live', $output[0]['gateway_account'] );
-		$this->assertSame( 'po_123', $output[0]['settlement_batch_reference'] );
-		$this->assertArrayNotHasKey( 'balance_transaction_id', $output[0] );
-		$this->assertSame( 'payout', $output[1]['type'] );
-	}
-
 	public function testParsePaymentsActivityCsv(): void {
 		$processor = new StripeAudit();
 		$output = $processor->parseFile( __DIR__ . '/../Data/payments_activity.csv' );
